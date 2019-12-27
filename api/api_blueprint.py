3b68ef03aed44dd2123dca07e587c534db6c95f2
Ubuntu <ubuntu@ip-172-31-33-1.ap-northeast-1.compute.internal>
2019-12-27 06:51:49 +0000

    modified endpoints, added http return code, auth temporarily removed

--- api/api_blueprint.py
+++ api/api_blueprint.py
@@ -8,8 +8,21 @@ from api import items
 API_BLUEPRINT = Blueprint('api', __name__, url_prefix='/')
 
 ITEM_VIEW = items.ItemAPI.as_view('items')
+
+url = "user/<int:user_id>/items/"
 API_BLUEPRINT.add_url_rule(
-    'items',
+    url, view_func=ITEM_VIEW,
+    methods=['GET'],
+    defaults={"item_id": None}
+)
+
+API_BLUEPRINT.add_url_rule(
+    url, view_func=ITEM_VIEW,
+    methods=['POST']
+)
+
+API_BLUEPRINT.add_url_rule(
+    '%s<int:item_id>' % url,
     view_func=ITEM_VIEW,
-    methods=['POST', 'PUT', 'GET', 'DELETE']
+    methods=['PUT', 'GET', 'DELETE']
 )
--- api/items.py
+++ api/items.py
@@ -13,78 +13,63 @@ class ItemAPI(MethodView):
     """
     items CRUD endpoints
     """
-    def get(self):
+    def get(self, user_id, item_id):
         """query items"""
-        ret = auth_util.auth_check(request)
-        if isinstance(ret, str):
-            return auth_util.make_json_response('fail', ret)
+        if item_id is None:
+            elements = list_item.Item.query.filter_by(owner_id=user_id).all()
+        else:
+            elements = list_item.Item.query.filter_by(owner_id=user_id,
+                                                      id=item_id).all()
+        return auth_util.make_json_response('sucess', repr(elements), 200)
 
-        id_int = int(ret['sub'])
-
-        elements = list_item.Item.query.filter_by(owner_id=id_int).all()
-        return auth_util.make_json_response('sucess', repr(elements))
-
-    def post(self): # pylint: disable=no-self-use
+    def post(self, user_id): # pylint: disable=no-self-use
         """add items"""
-        ret = auth_util.auth_check(request)
-        if isinstance(ret, str):
-            return auth_util.make_json_response('fail', ret)
-        #auth ok, do insert
         request_body = request.get_json()
         title_str = request_body.get('title')
         context_str = request_body.get('context')
 
-        id_int = int(ret['sub'])
-
         ele = list_item.Item.query.filter_by(title=title_str,
-                                             owner_id=id_int).first()
+                                             owner_id=user_id).first()
         if ele:
-            return auth_util.make_json_response('fail', 'title already exists')
+            return auth_util.make_json_response('fail',
+                                                'item already exists',
+                                                409)
         new_item = list_item.Item(title=title_str,
                                   context=context_str,
-                                  owner_id=id_int)
+                                  owner_id=user_id)
         sqlalchemy_db.db.session.add(new_item)
         sqlalchemy_db.db.session.commit()
-        return auth_util.make_json_response('sucess', str(new_item) + ' added')
+        return auth_util.make_json_response('sucess', str(new_item.id) +
+                                            ' added: ' + repr(new_item), 200)
 
-    def delete(self):
+    def delete(self, user_id, item_id):
         """delete items"""
-        ret = auth_util.auth_check(request)
-        if isinstance(ret, str):
-            return auth_util.make_json_response('fail', ret)
-        request_body = request.get_json()
-        title_str = request_body.get('title')
-
-        id_int = int(ret['sub'])
-
-        ele = list_item.Item.query.filter_by(title=title_str,
-                                             owner_id=id_int).first()
+        ele = list_item.Item.query.filter_by(id=item_id,
+                                             owner_id=user_id).first()
         if not ele:
-            return auth_util.make_json_response('fail', 'title not exists')
+            return auth_util.make_json_response('fail', 'item not exists', 404)
         sqlalchemy_db.db.session.delete(ele)
         sqlalchemy_db.db.session.commit()
-        return auth_util.make_json_response('sucess', repr(ele) + 'deleted')
+        return auth_util.make_json_response('sucess',
+                                            repr(ele) + 'deleted', 200)
 
-    def put(self):
+    def put(self, user_id, item_id):
         """update items"""
-        ret = auth_util.auth_check(request)
-        if isinstance(ret, str):
-            return auth_util.make_json_response('fail', ret)
         request_body = request.get_json()
         title_str = request_body.get('title')
         context_str = request_body.get('context')
 
-        id_int = int(ret['sub'])
-
-        ele = list_item.Item.query.filter_by(title=title_str,
-                                             owner_id=id_int).first()
+        ele = list_item.Item.query.filter_by(id=item_id,
+                                             owner_id=user_id).first()
         if ele:
             #when title already exists, update item
+            ele.title = title_str
             ele.context = context_str
         else:
             #when title not exists, add item
             ele = list_item.Item(title=title_str,
-                                 context=context_str, owner_id=id_int)
+                                 context=context_str, owner_id=user_id)
             sqlalchemy_db.db.session.add(ele)
         sqlalchemy_db.db.session.commit()
-        return auth_util.make_json_response('sucess', str(ele) + ' updated')
+        return auth_util.make_json_response('sucess', repr(ele) + ' updated',
+                                            200)
